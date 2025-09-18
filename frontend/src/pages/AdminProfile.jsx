b48ed021add964c4fb1e090efcfa042ffba6f7ab
mhrafi39 <mehedihasanrafi39@gmail.com>
2025-09-18 22:19:14 +0600

    ff

--- frontend/src/pages/AdminProfile.jsx
+++ frontend/src/pages/AdminProfile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaUser, FaEnvelope, FaShieldAlt } from "react-icons/fa";
 import { Settings, ChevronDown, Trash2 } from "lucide-react";
-import "../Styles/AdminProfile.css";
+import "../styles/AdminProfile.css";
 import { API_BASE } from "../api";
 
 const AdminProfile = () => {
